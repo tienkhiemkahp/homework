fd943784a7036d46f21d62831850b391a9a10974
unknown <tienkhiemkahp2k1@gmail.com>
2024-08-11 15:27:40 +0700

    chuaxong

--- Day-44/portfolio-page/src/App.jsx
+++ Day-44/portfolio-page/src/App.jsx
@@ -5,7 +5,7 @@ import Section1 from './components/section1/Section1'
 import Section2 from './components/section2/Section2'
 import Section3 from './components/section3/Section3'
 import Section4 from './components/section4/Section4'
-import Section5 from './components/section5/section5'
+import Section5 from './components/section5/Section5'
 import Section6 from './components/section6/Section6'
 export default function App() {
   return (
